4ac7396aab876e16e192e2dff1debfc552beb5b5
Fabio Vettori <fabio_vettori@hotmail.it>
2021-01-07 16:30:54 +0100

    header ok

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,3 +1,68 @@
 <header>
-    {{-- header content --}}
+    <section id="top-banner">
+        <div class="container-lg">
+            <div class="row">
+                <div class="col-lg-12 d-flex justify-content-end">
+                    <a class="text-uppercase" href="#">
+                        <span>dc power
+                            <span class="marker">&#8480;</span>
+                        </span>
+                        <span class="ml-1">visa
+                            <span class="marker">&#174;</span>
+                        </span>
+                    </a>
+                    <a class="text-uppercase" href="#">additional dc sites
+                        <span class="fas fa-sort-down marker ml-1"></span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <nav>
+        <div class="container-lg">
+            <div class="row">
+                <div class="col-lg-2 d-flex justify-content-center">
+                    <img src=" {{ asset('images/dc-logo.png') }} " alt="dc comics logo">
+                </div>
+
+                <ul class="col-lg-8 d-flex m-0 list-unstyled justify-content-around align-center">
+                    <li>
+                        <a href="#">characters</a>
+                    </li>
+                    <li>
+                        <a href="#">comics</a>
+                    </li>
+                    <li>
+                        <a href="#">movies</a>
+                    </li>
+                    <li>
+                        <a href="#">tv</a>
+                    </li>
+                    <li>
+                        <a href="#">games</a>
+                    </li>
+                    <li>
+                        <a href="#">collectibles</a>
+                    </li>
+                    <li>
+                        <a href="#">videos</a>
+                    </li>
+                    <li>
+                        <a href="#">fans</a>
+                    </li>
+                    <li>
+                        <a href="#">news</a>
+                    </li>
+                    <li>
+                        <a href="#">shop</a>
+                    </li>
+                </ul>
+
+                <div class="col-lg-2 d-flex align-center">
+                    <input type="text" placeholder="search">
+                </div>
+            </div>
+        </div>
+    </nav>
 </header>
