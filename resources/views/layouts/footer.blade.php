a46dd6a289fa246ada6c35d2ed4aaef2dd6a9dd2
Fabio Vettori <fabio_vettori@hotmail.it>
2021-01-07 19:39:06 +0100

    footer-bottom ok

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,40 @@
 <footer>
-    {{-- footer content --}}
+    <section id="footer-top">
+        <div class="container-lg">
+            <div class="row">
+                <div class="col-lg-4">
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="footer-bottom">
+        <div class="container-lg">
+            <div class="row">
+                <div class="col-lg-6">
+                    <a class="text-uppercase cta" href="#">sign up now!</a>
+                </div>
+
+                <div class="col-lg-6 social">
+                    <span class="text-uppercase">follow us</span>
+                    <a href="#">
+                        <img src=" {{ asset('images/footer-facebook.png') }} " alt="facebook">
+                    </a>
+                    <a href="#">
+                        <img src=" {{ asset('images/footer-twitter.png') }} " alt="twitter">
+                    </a>
+                    <a href="#">
+                        <img src=" {{ asset('images/footer-youtube.png') }} " alt="youtube">
+                    </a>
+                    <a href="#">
+                        <img src=" {{ asset('images/footer-pinterest.png') }} " alt="pintarest">
+                    </a>
+                    <a href="#">
+                        <img src=" {{ asset('images/footer-periscope.png') }} " alt="periscope">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
 </footer>
