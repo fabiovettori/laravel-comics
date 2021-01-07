d078ce9d600adf58f77ec9865d14862d4bb56f02
Fabio Vettori <fabio_vettori@hotmail.it>
2021-01-07 21:42:44 +0100

    footer-top ok

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,8 +2,100 @@
     <section id="footer-top">
         <div class="container-lg">
             <div class="row">
-                <div class="col-lg-4">
+                <div class="col-lg-6">
+                    <div class="row">
+                        <ul class="col-lg-4 list-unstyled">
+                            <li class="text-uppercase">dc comics</li>
+                            <li>
+                                <a href="#">characters</a>
+                            </li>
+                            <li>
+                                <a href="#">comics</a>
+                            </li>
+                            <li>
+                                <a href="#">movies</a>
+                            </li>
+                            <li>
+                                <a href="#">tv</a>
+                            </li>
+                            <li>
+                                <a href="#">games</a>
+                            </li>
+                            <li>
+                                <a href="#">videos</a>
+                            </li>
+                            <li>
+                                <a href="#">news</a>
+                            </li>
+                        </ul>
 
+                        <ul class="col-lg-4 list-unstyled">
+                            <li class="text-uppercase">dc</li>
+                            <li>
+                                <a href="#">terms of use</a>
+                            </li>
+                            <li>
+                                <a href="#">privacy policy (new)</a>
+                            </li>
+                            <li>
+                                <a href="#">advertising</a>
+                            </li>
+                            <li>
+                                <a href="#">jobs</a>
+                            </li>
+                            <li>
+                                <a href="#">subscriptions</a>
+                            </li>
+                            <li>
+                                <a href="#">talent workshop</a>
+                            </li>
+                            <li>
+                                <a href="#">cpsc certificates</a>
+                            </li>
+                            <li>
+                                <a href="#">ratings</a>
+                            </li>
+                            <li>
+                                <a href="#">shop help</a>
+                            </li>
+                            <li>
+                                <a href="#">contact us</a>
+                            </li>
+                        </ul>
+
+                        <ul class="col-lg-4 list-unstyled">
+                            <li class="text-uppercase">sites</li>
+                            <li>
+                                <a href="#">dc</a>
+                            </li>
+                            <li>
+                                <a href="#">mad magazine</a>
+                            </li>
+                            <li>
+                                <a href="#">dc kids</a>
+                            </li>
+                            <li>
+                                <a href="#">dc universe</a>
+                            </li>
+                            <li>
+                                <a href="#">dc power visa</a>
+                            </li>
+                        </ul>
+
+                        <ul class="col-lg-4 list-unstyled">
+                            <li class="text-uppercase">shop</li>
+                            <li>
+                                <a href="#">shop dc</a>
+                            </li>
+                            <li>
+                                <a href="#">shop dc collectibles</a>
+                            </li>
+                        </ul>
+
+                        <div class="col-lg-12">
+                            <p>All Site Content TM and © 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved. <a href="#">Cookies Settings</a> </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
